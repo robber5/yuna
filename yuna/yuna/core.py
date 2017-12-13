a62a70a587b68ba7e48f5199d2888c617cc0b1e3
智 吕 <luolihaokong@hotmail.com>
2017-12-13 11:06:25 +0800

    单个股票查询当日收盘，数据库清理

--- yuna/yuna/core.py
+++ yuna/yuna/core.py
@@ -2,27 +2,29 @@ from packages import pymysql
 import WindPy
 import re
 import datetime
+from functools import partial
 
 __title__ = 'yuna'
 __version__ = '0.0.2'
 __author__ = 'lvzhi'
 __copyright__ = 'Copyright 2017 lvzhi'
 
+'''连接数据库相关常量'''
+HOST = 'localhost'
+PORT = 3306
+USER = 'root'
+PASS_WD = 'lvzhi'
+DB = 'yuna'
 
-def main():
-    pass
 
-
-def update(stock=600068, date=None):
+def _update(conn, stock, date=1):
     """周六日无法更新"""
     if datetime.date.today().weekday() in (5, 6):
         return 'None'
-
+    stock_name = stock + '.sh' if stock[0] == '6' else stock + '.sz'
     WindPy.w.start()
-    conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='lvzhi', db='yuna',
-                           charset='utf8')
     cur = conn.cursor()
-    a = WindPy.w.wsd('600068.sh', 'close')
+    a = WindPy.w.wsd(stock_name, 'close')
     b = a.Times[0].timetuple()
     c = a.Data[0][0]
     d = a.Codes[0]
@@ -30,17 +32,28 @@ def update(stock=600068, date=None):
         cur.execute('create table `{}` (time date not null, price float not null)'.format(d[:-3]))
     except pymysql.err.InternalError:
         pass
-    if not cur.execute('select * from `{}` where time = {}{}{}'.format(d[:-3], b[0], b[1], __date_update(b[2]))):
-        cur.execute('insert into `{}` values ({}{}{}, {})'.format(d[:-3], b[0], b[1],__date_update(b[2]), c))
+    if not cur.execute('select * from `{}` where time = {}{}{}'.format(d[:-3], b[0], b[1], _date_update(b[2]))):
+        cur.execute('insert into `{}` values ({}{}{}, {})'.format(d[:-3], b[0], b[1], _date_update(b[2]), c))
     else:
         pass
     conn.commit()
     cur.close()
-    conn.close()
     return 'OK'
 
 
-def __date_update(original_date):
+def _delete(conn):
+    cur = conn.cursor()
+    cur.execute("select concat('drop table `', table_name, '`;') from information_schema.tables where table_schema = "
+                "'{}'".format(DB))
+    var = cur.fetchall()
+    length = len(var)
+    for i in range(length):
+        cur.execute("{}".format(var[i][0]))
+    conn.commit()
+    cur.close()
+
+
+def _date_update(original_date):
     """把类似2这样的单个数字转化为02，而双数字则保持不变"""
     return re.sub(r'(\b[1-9]\b)', r'0\1', str(original_date))
 
@@ -54,4 +67,8 @@ def _ma():
 
 
 if __name__ == '__main__':
-    main()
+    conn = pymysql.connect(host=HOST, port=PORT, user=USER, passwd=PASS_WD, db=DB,
+                           charset='utf8')
+    update = partial(_update, conn)
+    delete = partial(_delete, conn)
+    """conn.close()"""
