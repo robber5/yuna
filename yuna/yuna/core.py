73bd4229f6802f5fac1ecc74ce3f786a153f52c4
lvzhi <279094345@qq.com>
2017-12-27 16:30:57 +0800

    初步实现相对强弱指标（RSI）以及移动平均线（SMA）

--- yuna/yuna/core.py
+++ yuna/yuna/core.py
@@ -89,6 +89,44 @@ class MyMacd(TechnicalIndicator):
         self.ans.append(macd.ans)
 
 
+class Sma(TechnicalIndicator):
+    """移动平均线"""
+
+    def __init__(self, data, days, factor):
+        TechnicalIndicator.__init__(self, data)
+        if days <= factor: raise ValueError("天数必须大于权重参数")
+        self.days = days
+        self.weight_factor = factor / days
+        self._handle()
+
+    def _handle(self):
+        """self.ans = [ans]"""
+        ans_length = len(self.data)
+        for one in range(ans_length):
+            if one == 0:
+                self.ans.append(self.data[one])
+            else:
+                self.ans.append(self.weight_factor * self.data[one] + (1 - self.weight_factor) * self.ans[-1])
+
+    def __sub__(self, other):
+        ans, ans_length = [], len(self.ans)
+        for one in range(ans_length):
+            ans.append(self.ans[one] - other.ans[one])
+        obj = Ema(ans, 1)
+        return obj
+
+    def __mul__(self, other):
+        obj = Ema(list(map(lambda x: x * other, self.ans)), 1)
+        return obj
+
+    def __truediv__(self, other):
+        ans, ans_length = [], len(self.ans)
+        for one in range(ans_length):
+            ans.append(self.ans[one] / other.ans[one])
+        obj = Ema(ans, 1)
+        return obj
+
+
 class Ema(TechnicalIndicator):
     """指数移动平均线"""
 
@@ -136,6 +174,33 @@ class Macd(TechnicalIndicator):
         self.ans.append(dea.ans)
         macd = (diff - dea) * 2
         self.ans.append(macd.ans)
+
+
+class Rsi(TechnicalIndicator):
+    """相对强弱指标"""
+    def __init__(self, data, n1, n2, n3):
+        TechnicalIndicator.__init__(self, data)
+        self.N1 = n1
+        self.N2 = n2
+        self.N3 = n3
+        self._handle()
+
+    def _max(self, opt_list):
+        return list(map(lambda x: max(x, 0), opt_list))
+
+    def _abs(self, opt_list):
+        return list(map(lambda x: abs(x), opt_list))
+
+    def _handle(self):
+        opt_list, opt_list_length = [], len(self.data) - 1
+        for one in range(opt_list_length):
+            opt_list.append(self.data[one + 1] - self.data[one])
+        rsi1 = Sma(self._max(opt_list), self.N1, 1) / Sma(self._abs(opt_list), self.N1, 1) * 100
+        self.ans.append(rsi1)
+        rsi2 = Sma(self._max(opt_list), self.N2, 1) / Sma(self._abs(opt_list), self.N2, 1) * 100
+        self.ans.append(rsi2)
+        rsi3 = Sma(self._max(opt_list), self.N3, 1) / Sma(self._abs(opt_list), self.N3, 1) * 100
+        self.ans.append(rsi3)
 
 
 def _update(conn, stocks, date=1):
